7ea9938d1d3ce4de6cd2d0cac9679da2e113afc3
Trushil <trushil.budhia@gmail.com>
2018-09-06 20:25:43 +0800

    Initial Commit

--- src/actions/randomNumber.js
+++ src/actions/randomNumber.js
@@ -1,27 +1,10 @@
 
 export const GENERATE_RANDOMNUMBER = 'GENERATE_RANDOMNUMBER'
 export function generateRandomNumber() {
-  const RandomNumber = Math.floor(Math.random() * 3) + 1
-  let result = "-";
-	 
-  if (RandomNumber === 1) {
-    result = "Rock";
-  };
-
-  if (RandomNumber === 2) {
-    result = "Paper";
-  };
-  
-  if (RandomNumber === 3) {
-    result = "Scissor";
-  };
-
   return {
 	  type:  GENERATE_RANDOMNUMBER,
-	  number: result
+	  number: Math.floor(Math.random() * 100) + 1
   }
-
-  
 }
 
 
